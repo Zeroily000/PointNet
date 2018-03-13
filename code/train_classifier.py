58768e9e7fc1bba12ca48831d9c0b7a2eea28525
Zeroily000 <zhz362@ucsd.edu>
2018-03-12 20:03:41 -0700

    add data augmentation

--- code/train_classifier.py
+++ code/train_classifier.py
@@ -2,10 +2,52 @@ import torch
 from architecture import PointNetClassification
 import torch.optim.lr_scheduler
 import os
+import math
 
-# test dimension
-if __name__ == '__main__':
 
+def rot_mat(num_images):
+    # roll = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+    # Rx = torch.cat(
+    #     (torch.cat((torch.ones(num_images, 1, 1), torch.zeros(num_images, 1, 1), torch.zeros(num_images, 1, 1)), 2),
+    #      torch.cat((torch.zeros(num_images, 1, 1), torch.cos(roll), -torch.sin(roll)), 2),
+    #      torch.cat((torch.zeros(num_images, 1, 1), torch.sin(roll), torch.cos(roll)), 2)), 1)
+    #
+    # pitch = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+    # Ry = torch.cat((torch.cat((torch.cos(pitch), torch.zeros(num_images, 1, 1), torch.sin(pitch)), 2),
+    #                 torch.cat(
+    #                     (torch.zeros(num_images, 1, 1), torch.ones(num_images, 1, 1), torch.zeros(num_images, 1, 1)),
+    #                     2),
+    #                 torch.cat((-torch.sin(pitch), torch.zeros(num_images, 1, 1), torch.cos(pitch)), 2)), 1)
+
+    yaw = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+    Rz = torch.cat((torch.cat((torch.cos(yaw), -torch.sin(yaw), torch.zeros(num_images, 1, 1)), 2),
+                    torch.cat((torch.sin(yaw), torch.cos(yaw), torch.zeros(num_images, 1, 1)), 2),
+                    torch.cat((torch.zeros(num_images, 1, 1), torch.zeros(num_images, 1, 1), torch.ones(num_images, 1, 1)),2)), 1)
+
+    # return torch.bmm(Rx, torch.bmm(Ry, Rz))
+    return Rz
+
+
+def eval_acc(X, t, classifier):
+    batch_size = 32
+    batch_num = X.size(0) / batch_size
+    # train Accuracy
+    correct = 0
+    for bn in range(batch_num):
+        X_batch = torch.autograd.Variable(X[bn * batch_size: bn * batch_size + batch_size, :, :])
+        t_batch = t[bn * batch_size: bn * batch_size + batch_size]
+        if torch.cuda.is_available():
+            X_batch = X_batch.cuda()
+        y_batch = classifier(X_batch).data.max(1)[1]
+        # outputs = classifier(X_batch)
+        # _, y_batch = torch.max(outputs.data, 1)
+        correct += (y_batch.cpu() == t_batch).sum()
+
+    return correct * 1.0 / X.size(0)
+
+
+def train_classifier(learning_rate=0.001, regularization=0.001, reshuffle=True,
+                     batch_size=32, step_size = 20, annealing=0.5):
     data_dir = '../dataset/ModelNet40'
     # catset = set([os.path.join(f) for f in os.listdir(dataset)])
     # cat2lab = {c: i for c, i in zip(catset, xrange(len(catset)))}
@@ -19,65 +61,48 @@ if __name__ == '__main__':
     labels_train = torch.load(os.path.join(data_dir, 'labels_train.pth'))
     labels_test = torch.load(os.path.join(data_dir, 'labels_test.pth'))
 
-    num_images = data_train.shape[0]
-    X_train = data_train[num_images/5:, :, :]
-    t_train = labels_train[num_images/5:]
+    # augmentation
+    augment = 4
+    data_train = torch.cat([data_train] + [torch.bmm(R, data_train) for R in [rot_mat(data_train.size(0)) for i in xrange(augment)]], 0)
+    data_train += torch.normal(means=torch.zeros(data_train.size()), std=0.02*torch.ones(data_train.size()))
+    # data_train += torch.normal(means=0, std=0.02 * torch.ones(data_train.size()))
+    labels_train = labels_train.repeat(augment + 1)
+    print 'Done'
 
-    X_valid = data_train[:num_images/5, :, :]
-    t_valid = labels_train[num_images/5:]
+    num_images = data_train.size(0)
+    X_train = data_train[num_images / 5:, :, :]
+    t_train = labels_train[num_images / 5:]
+
+    X_valid = data_train[:num_images / 5, :, :]
+    t_valid = labels_train[num_images / 5:]
 
     X_test = data_test
     t_test = labels_test
-    print 'Done'
-
-
-
-
-
-    # batch_num = len(data_train)
-    # if torch.cuda.is_available():
-    #     X_train = torch.autograd.Variable(torch.FloatTensor(data_train[batch_num/5:]).transpose(-1,-2).type(torch.cuda.FloatTensor))
-    #     X_valid = torch.autograd.Variable(torch.FloatTensor(data_train[:batch_num/5]).transpose(-1,-2).type(torch.cuda.FloatTensor))
-    #     t_train = torch.autograd.Variable(torch.LongTensor(labels_train[batch_num/5:]).type(torch.cuda.LongTensor))
-    #     t_valid = torch.autograd.Variable(torch.LongTensor(labels_train[:batch_num/5]).type(torch.cuda.LongTensor))
-    #
-    #     X_test = torch.autograd.Variable(torch.FloatTensor(data_test).type(torch.cuda.FloatTensor).transpose(-1,-2))
-    #     t_test = torch.autograd.Variable(torch.LongTensor(labels_test).type(torch.cuda.LongTensor))
-    #
-    # else:
-    #     X_train = torch.autograd.Variable(torch.FloatTensor(data_train[batch_num / 5:]).transpose(-1,-2))
-    #     X_valid = torch.autograd.Variable(torch.FloatTensor(data_train[:batch_num / 5]).transpose(-1,-2))
-    #     t_train = torch.autograd.Variable(torch.LongTensor(labels_train[batch_num / 5:]))
-    #     t_valid = torch.autograd.Variable(torch.LongTensor(labels_train[:batch_num / 5]))
-    #
-    #     X_test = torch.autograd.Variable(torch.FloatTensor(data_test).transpose(-1,-2))
-    #     t_test = torch.autograd.Variable(torch.LongTensor(labels_test))
-
 
     pn_classify = PointNetClassification(num_classes=40)
     if torch.cuda.is_available():
         pn_classify = pn_classify.cuda()
 
 
-    #
-    # learning_rate = 0.001
-    # momentum = 0.9
-    # batch_size = 32
-    # annealing = 2.0
-    optimizer = torch.optim.SGD(params=pn_classify.parameters() , lr=0.001, momentum=0.9, weight_decay=0.001, nesterov=True)
+    optimizer = torch.optim.Adam(params=pn_classify.parameters(), lr=learning_rate, weight_decay=regularization)
+    # optimizer = torch.optim.SGD(params=pn_classify.parameters(), lr=0.001, momentum=0.9, weight_decay=0.001,
+    #                             nesterov=True)
     criterion = torch.nn.CrossEntropyLoss()
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=annealing)
 
-    batch_size = 32
-    batch_num = X_train.shape[0] / batch_size
-
+    num_images = X_train.size(0)
+    batch_num = num_images / batch_size
+    idx = torch.LongTensor(range(num_images))
     for epoch in xrange(500):
         scheduler.step()
         pn_classify.train(True)
         loss_epoch = 0
+
+        if reshuffle:
+            idx = torch.randperm(num_images)
         for bn in xrange(batch_num):
-            X_batch = torch.autograd.Variable(X_train[bn*batch_size: bn*batch_size+batch_size, :, :])
-            t_batch = torch.autograd.Variable(t_train[bn*batch_size: bn*batch_size+batch_size])
+            X_batch = torch.autograd.Variable(X_train[idx[bn * batch_size: bn * batch_size + batch_size], :, :])
+            t_batch = torch.autograd.Variable(t_train[idx[bn * batch_size: bn * batch_size + batch_size]])
             if torch.cuda.is_available():
                 X_batch = X_batch.cuda()
                 t_batch = t_batch.cuda()
@@ -91,12 +116,17 @@ if __name__ == '__main__':
 
             loss_epoch += loss.data[0] / batch_num
 
-            if bn%100 == 0:
-                print loss.data[0]
-
-
+        pn_classify.train(False)
         print epoch, ':', loss_epoch
+        print 'train acc:', eval_acc(X_train, t_train, pn_classify)
+        print 'validation acc:', eval_acc(X_valid, t_valid, pn_classify)
+        print 'test acc:', eval_acc(X_test, t_test, pn_classify)
 
 
 
+# test dimension
+if __name__ == '__main__':
+    torch.manual_seed(19270817)
+    torch.cuda.manual_seed_all(19270817)
+    train_classifier(learning_rate=0.001, regularization=0.001, reshuffle=True, batch_size=32, step_size=20, annealing=0.5)
 
--- code/utils.py
+++ code/utils.py
@@ -24,8 +24,28 @@ def read_off(fpath, num_points):
     f.close()
     return points
 
+#
+# def rot_mat(num_images):
+#     roll = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+#     Rx = torch.cat((torch.cat((torch.ones(num_images, 1, 1), torch.zeros(num_images, 1, 1), torch.zeros(num_images, 1, 1)), 2),
+#                     torch.cat((torch.zeros(num_images, 1, 1), torch.cos(roll), -torch.sin(roll)), 2),
+#                     torch.cat((torch.zeros(num_images, 1, 1), torch.sin(roll), torch.cos(roll)), 2)), 1)
+#
+#     pitch = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+#     Ry = torch.cat((torch.cat((torch.cos(pitch), torch.zeros(num_images, 1, 1), torch.sin(pitch)), 2),
+#                     torch.cat((torch.zeros(num_images, 1, 1), torch.ones(num_images, 1, 1), torch.zeros(num_images, 1, 1)), 2),
+#                     torch.cat((-torch.sin(pitch), torch.zeros(num_images, 1, 1), torch.cos(pitch)), 2)), 1)
+#
+#     yaw = torch.Tensor(num_images, 1, 1).uniform_(0, math.pi)
+#     Rz = torch.cat((torch.cat((torch.cos(yaw), -torch.sin(yaw), torch.zeros(num_images, 1, 1)), 2),
+#                     torch.cat((torch.sin(yaw), torch.cos(yaw), torch.zeros(num_images, 1, 1)), 2),
+#                     torch.cat((torch.zeros(num_images, 1, 1), torch.zeros(num_images, 1, 1), torch.ones(num_images, 1, 1)), 2)), 1)
+#
+#     return torch.bmm(Rx, torch.bmm(Ry, Rz))
+#
 
-def prepare_data(dataset, cat2lab, dataset_type = '', num_points = 2048, reshuffle = True):
+
+def prepare_data(dataset, cat2lab, dataset_type = '', num_points = 1024, reshuffle = True):
     '''
     Load all data
     :param dataset: folder path
@@ -34,13 +54,11 @@ def prepare_data(dataset, cat2lab, dataset_type = '', num_points = 2048, reshuff
     '''
 
     # load train file paths
-    # paths = [os.path.join(root, filename) for root, _, files in os.walk(dataset, topdown=False) for filename in files if dataset_type in root]
     paths = [(root, filename) for root, _, files in os.walk(dataset, topdown=False) for filename in files if
              dataset_type in root]
 
     # random shuffle
     if reshuffle:
-        random.seed(19260817)
         random.shuffle(paths)
 
     data, labels = [], []
@@ -50,10 +68,6 @@ def prepare_data(dataset, cat2lab, dataset_type = '', num_points = 2048, reshuff
             data.append(points)
             labels.append(cat2lab[root.split('/')[-2]])
 
-        # delete later
-        # if len(data) >= temp:
-        #     break
-        # end
     data = torch.FloatTensor(data)  # num_images x num_points x 3
 
     # zero-center
@@ -62,73 +76,6 @@ def prepare_data(dataset, cat2lab, dataset_type = '', num_points = 2048, reshuff
     # normalize
     data /= torch.max(data.norm(p=2, dim=-1, keepdim=True), dim=1, keepdim=True)[0]
 
-
-    # maps category(string) to label(int)
-    # labels = torch.LongTensor([cat2lab[c] for c in cats])  # num_images
-
-
-    # paths_test = [os.path.join(root, filename) for root, _, files in os.walk(dataset, topdown=False) for filename in
-    #               files if 'test' in root]
-    #
-    # data_test, cats_test = [], []
-    # for fp in paths_test:
-    #     points = read_off(fp, num_points)
-    #     if len(points) != 0:
-    #         data_test.append(points)
-    #         cats_test.append(fp.split('_')[0].split('/')[-1])
-    #
-    #     # delete later
-    #     if len(data_test) >= temp:
-    #         break
-    #     # end
-    # data_test = torch.FloatTensor(data_test)  # num_images x num_points x 3
-    #
-    # # zero-center
-    # data_test -= (torch.max(data_test, dim=-2, keepdim=True)[0] + torch.min(data_test, dim=-2, keepdim=True)[0]) / 2
-    #
-    # # normalize
-    # data_test /= torch.max(data_test.norm(p=2, dim=-1, keepdim=True), dim=1, keepdim=True)[0]
-
-    # data_batch = []
-    # data_train, cats_train = [], []
-    # for fp in paths_train:
-    #     if len(data_batch) < batch_size:
-    #         points = read_off(fp, num_points)
-    #         if len(points) != 0:
-    #             data_batch.append(points)
-    #             cats_train.append(fp.split('_')[0].split('/')[-1])
-    #     else:
-    #         data_train.append(data_batch)
-    #         data_batch = []
-    #
-    #     if batch_num and len(data_train) >= batch_num:
-    #         break
-
-    # data_test, cats_test = [], []
-    # for fp in paths_test:
-    #     if len(data_batch) < batch_size:
-    #         points = read_off(fp, num_points)
-    #         if len(points) != 0:
-    #             data_batch.append(points)
-    #             cats_test.append(fp.split('_')[0].split('/')[-1])
-    #     else:
-    #         data_test.append(data_batch)
-    #         data_batch = []
-    #
-    #     if batch_num and len(data_test) >= batch_num:
-    #         break
-
-
-    # map string to integer
-    # cats = set(cats_train+cats_test)
-
-
-    # labels_test = torch.LongTensor([cat2lab[c] for c in cats_test]) # num_images
-    # labels_train = [[cat2lab[c] for c in cats_train[bn * batch_size: bn * batch_size + batch_size]] for bn in xrange(batch_num)]
-    # labels_test = [[cat2lab[c] for c in cats_test[bn * batch_size: bn * batch_size + batch_size]] for bn in xrange(batch_num)]
-
-
-    # return torch.FloatTensor(data_train), torch.FloatTensor(data_test), torch.LongTensor(labels_train), torch.LongTensor(labels_test)
     return data.transpose(-1, -2), torch.LongTensor(labels)
 
 def data_visualization(points):
@@ -138,14 +85,17 @@ def data_visualization(points):
     plt.show()
 
 if __name__ == '__main__':
+    random.seed(19260817)
+    torch.manual_seed(19260817)
+
     dataset = '../dataset/ModelNet40'
     catset = set([os.path.join(f) for f in os.listdir(dataset)])
     cat2lab = {c: i for c, i in zip(catset, xrange(len(catset)))}
 
 
     since = time.time()
-    data_train, labels_train = prepare_data(dataset=dataset, cat2lab=cat2lab, dataset_type='train', num_points=2048, reshuffle=True)
-    data_test, labels_test = prepare_data(dataset=dataset, cat2lab=cat2lab, dataset_type='test', num_points=2048, reshuffle=True)
+    data_train, labels_train = prepare_data(dataset=dataset, cat2lab=cat2lab, dataset_type='train', num_points=1024, reshuffle=True)
+    data_test, labels_test = prepare_data(dataset=dataset, cat2lab=cat2lab, dataset_type='test', num_points=1024, reshuffle=True)
     print 'load raw data takes', time.time() - since, 's'
 
     since = time.time()
@@ -156,7 +106,4 @@ if __name__ == '__main__':
     torch.save(labels_test, os.path.join(dataset, 'labels_test.pth'))
     print 'save data takes', time.time() - since, 's'
 
-    # since = time.time()
-    # data_test = torch.load(os.path.join(dataset, 'data_train.pth'))
-    # print 'load torch.tensor data takes', time.time() - since, 's'
-    # data_visualization(np.array(data_train[0][1], dtype=float).T)
+
